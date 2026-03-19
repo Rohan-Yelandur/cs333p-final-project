3558641eba0a04f873d6e841c967b41e96042c26
Rohan-Yelandur <ry7games@gmail.com>
2026-03-18 23:54:41 -0500

    Add timing to baseline
    Also created a header file with definitions for all iterations of the simulator. Added a function that picks random numbers from a normal distribution using the Box-Muller transform

--- Makefile
+++ Makefile
@@ -1,6 +1,3 @@
 baseline:
 	gcc src/baseline.c -o baseline.o -O0
 	./baseline.o
-
-clean:
-	rm -f baseline.o
--- src/baseline.c
+++ src/baseline.c
@@ -1,41 +1,46 @@
-#include <stdio.h>
-#include <math.h>
+#include "simulator.h"
 
-#define PATHS       1000000 // Number of simulation paths
-#define DAYS        252     // Trading days per year
-#define T           1       // Time to maturity (in years)
-#define S0          100.0   // Starting price
-#define K           100.0   // Strike price
-#define SIGMA       0.2     // Volatility
-#define R           0.05    // Risk-free interest rate            
+double rand_normal () {
+    double u1 = (rand() + 1.0) / (RAND_MAX + 2.0);
+    double u2 = (double)rand() / RAND_MAX;
+    return sqrt(-2.0 * log(u1)) * cos(2.0 * PI * u2);
+}
 
-int simulate_path () {
-    double dt = T / DAYS;
-    double drift = R - pow(SIGMA, 2) / 2.0;
-    double diffusion = SIGMA * sqrt(T);
-    double day_price = S0;
-    double sum = 0.0;
-    
+double simulate_path() {
+    double dt        = T / DAYS;
+    double drift     = (R - 0.5 * SIGMA * SIGMA) * dt;
+    double diffusion = SIGMA * sqrt(dt);
+    double price     = S0;
+    double sum       = 0.0;
+
     for (int i = 0; i < DAYS; i++) {
-        // Geometric Brownian Motion
-        // price = price * exp(drift * T + diffusion) NEED TO IMPLEMENT RAND FOR NORMAL DISTRIBUTION
-        sum += day_price;
+        price = price * exp(drift + diffusion * rand_normal());
+        sum  += price;
     }
 
     return sum / DAYS;
 }
 
 int main () {
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    srand((unsigned int)time(NULL));
     double sum = 0.0;
 
     for (int i = 0; i < PATHS; i++) {
         double average_price = simulate_path();
-        double payoff = average_price > 0 ? average_price : 0;
+        double payoff = average_price - K > 0 ? average_price - K : 0;
         sum += payoff;
     }
 
     double option_price = (sum / PATHS) * exp(-R * T);
-    printf("Estimated Option Price: $%.6f", option_price);
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    
+    double time_taken = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+    printf("Elapsed Time: %f\n", time_taken);
+    printf("Estimated Option Price: $%f\n", option_price);
 
     return 0;
 }
